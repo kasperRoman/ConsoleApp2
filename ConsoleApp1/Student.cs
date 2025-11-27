49922e6ce77c7f3e7857a063aad8075cbdd029e3
Roman <kaspryshyn_roman@ukr.net>
2025-11-27 11:15:18 +0200

    add student

---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+
+        public string StudentName { get; private set; }
+        public int Age { get; private set; }
+
+        public Student (string studentName, int age)
+        {
+            StudentName = studentName;
+            Age = age;
+        }
+    }
+}
---
+++ ConsoleApp1/StudentDemo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class StudentDemo
+    {
+        public static void RunDemo()
+        {
+            List<Student> students = new List<Student>();
+     
+        }
+    }
+}
