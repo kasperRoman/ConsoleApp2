9fce0fad77658525e98cd0e960c85d62bfd6bcc7
Roman <kaspryshyn_roman@ukr.net>
2025-12-03 01:38:13 +0200

    a

---
+++ ConsoleLibrary/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLibrary
+{
+    public abstract class Book
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+
+        public bool IsBorrowed { get; private set; }
+
+        public Book(string title, string author)
+        {
+            Title = string.IsNullOrWhiteSpace(title) ? throw new ArgumentException("Title cannot be null or empty.") : title;
+            Author = string.IsNullOrWhiteSpace(author) ? throw new ArgumentException("Author cannot be null or empty.") : author;
+        }
+        public virtual BorrowResult Borrow()
+        { 
+           if (IsBorrowed)
+            {
+                return BorrowResult.AlreadyBorrowed;
+            }
+            IsBorrowed = true;
+            return BorrowResult.Success;
+        }
+
+    }
+   
+}
--- HttpClienDemo2/WeatherResponce.cs
+++ HttpClienDemo2/WeatherResponce.cs
@@ -1,33 +1,54 @@
-﻿namespace HttpClientDemo2
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace HttpClientDemo2
 {
     public class WeatherResponse
     {
-        public MainInfo? Main { get; set; }
-        public List<WeatherInfo>? Weather { get; set; }
-        public WindInfo? Wind { get; set; }
+        [JsonPropertyName("name")]
         public string? Name { get; set; }
+
+        [JsonPropertyName("sys")]
+        public Sys? Sys { get; set; }
+
+        // Зручна властивість, щоб не писати weather.Sys.Country всюди
+        [JsonIgnore]
         public string? Country => Sys?.Country;
-        public SysInfo? Sys { get; set; }
+
+        [JsonPropertyName("main")]
+        public MainInfo? Main { get; set; }
+
+        [JsonPropertyName("wind")]
+        public Wind? Wind { get; set; }
+
+        [JsonPropertyName("weather")]
+        public List<WeatherInfo>? Weather { get; set; }
+    }
+
+    public class Sys
+    {
+        [JsonPropertyName("country")]
+        public string? Country { get; set; }
     }
 
     public class MainInfo
     {
-        public float Temp { get; set; }
+        [JsonPropertyName("temp")]
+        public double Temp { get; set; }
+
+        [JsonPropertyName("humidity")]
         public int Humidity { get; set; }
+    }
+
+    public class Wind
+    {
+        [JsonPropertyName("speed")]
+        public double Speed { get; set; }
     }
 
     public class WeatherInfo
     {
+        [JsonPropertyName("description")]
         public string? Description { get; set; }
-    }
-
-    public class WindInfo
-    {
-        public float Speed { get; set; }
-    }
-
-    public class SysInfo
-    {
-        public string? Country { get; set; }
     }
 }
